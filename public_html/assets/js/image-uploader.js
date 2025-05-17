b33d081d629a4e9a7c654e2213b03763a4137cc1
Chirath <chirathtimodh@gmail.com>
2025-05-17 20:54:20 +0530

    feat: enable drag and drop files

--- public_html/assets/js/image-uploader.js
+++ public_html/assets/js/image-uploader.js
@@ -2,15 +2,12 @@ const imageUploader = document.querySelector(".image-uploader");
 
 if (imageUploader) {
   const hiddenFileInput = document.createElement("input");
-  const img = document.createElement("img");
+  const img = new Image();
   const defaultImageSrc = imageUploader.dataset.src
     ? imageUploader.dataset.src
     : "assets/images/image-upload.png";
   img.src = defaultImageSrc;
   img.alt = "Uploaded Image";
-  img.addEventListener("click", (event) => {
-    hiddenFileInput.click();
-  });
 
   imageUploader.appendChild(img);
   hiddenFileInput.id = imageUploader.dataset.id;
@@ -18,8 +15,46 @@ if (imageUploader) {
   hiddenFileInput.required = imageUploader.dataset.required == "true";
   hiddenFileInput.type = "file";
   hiddenFileInput.accept = "image/*";
+
+  imageUploader.appendChild(hiddenFileInput);
+
+  img.addEventListener("click", (event) => {
+    hiddenFileInput.click();
+  });
+  img.addEventListener("dragenter", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    imageUploader.classList.add("dragover");
+  });
+  img.addEventListener("dragover", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  });
+  img.addEventListener("drop", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const dt = event.dataTransfer;
+    const files = dt.files;
+    if (files.length > 0) {
+      hiddenFileInput.files = files;
+    }
+    handleFiles(files);
+    imageUploader.classList.remove("dragover");
+    console.log("Files dropped:", hiddenFileInput.files);
+    
+  });
+  img.addEventListener("dragleave", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    imageUploader.classList.remove("dragover");
+  });
+
   hiddenFileInput.addEventListener("change", (event) => {
-    const file = event.target.files[0];
+    handleFiles(event.target.files);
+  });
+
+  function handleFiles(files) {
+    const file = files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = function (e) {
@@ -27,7 +62,5 @@ if (imageUploader) {
       };
       reader.readAsDataURL(file);
     }
-  });
-
-  imageUploader.appendChild(hiddenFileInput);
+  }
 }
